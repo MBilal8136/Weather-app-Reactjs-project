1c5720f55d9bd87efd2d8dd67858284b39758b44
M Bilal <mbilal75362@gmail.com>
2025-07-23 19:09:59 +0500

    update and fix error

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -11,6 +11,7 @@ import PropTypes from 'prop-types';
 function InfoCard({ info }) {
 
 
+
     const Hot_Url = "https://media.istockphoto.com/id/998390080/photo/lavender-field-at-sunset.webp?a=1&b=1&s=612x612&w=0&k=20&c=b4BJWKaI-_0Gt5sHyyPVyZGH6w9P_dye0VdNYqUjJGY=";
     const cold_Url = "https://plus.unsplash.com/premium_photo-1668792545129-72d876248c1b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y29sZCUyMGltYWdlfGVufDB8fDB8fHww";
     const rain_Url = "https://media.istockphoto.com/id/1257951336/photo/transparent-umbrella-under-rain-against-water-drops-splash-background-rainy-weather-concept.webp?a=1&b=1&s=612x612&w=0&k=20&c=sw_CRZcGopaGHDWqtT1M8y64k5uCcq-nro55Bw3YzyQ=";
@@ -39,7 +40,7 @@ function InfoCard({ info }) {
          <p>Wind Gust = {info.Wind.gust}</p>
          <p>Wind Deg = {info.Wind.deg}</p>
          <p>Country = {info.Country}</p>
-         <p>The weather can be described as <i>{info.Weather}</i> and feels like {info.Temp}&deg;C</p>
+         <p>The weather can be described as <i>{info.Weather}</i> and feels like {info.FeelsLike}&deg;C</p>
         </Typography>
       </CardContent>
     </CardActionArea>
@@ -48,7 +49,5 @@ function InfoCard({ info }) {
 }
   InfoCard.propTypes= {
      info: PropTypes.func.isRequired,
-     name: PropTypes.string
-
 }
 export default InfoCard
